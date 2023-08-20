5bd06a4c4070453a8731276ea213df42903e607c
jerry048 <61510650+jerry048@users.noreply.github.com>
2023-08-21 04:56:23 +0800

    Update urm_install.py

--- urm_install.py
+++ urm_install.py
@@ -16,46 +16,42 @@ if not os.path.isdir(game_dir):
     exit(1)
 
 
-def main():
-    game_list = glob(game_dir + "\\*\\", recursive=True)
-    skip = 0
-    modded = 0
-    not_renpy = 0
-    for x in game_list:
-        game_name = x.split("\\")[-2]
-        dst_dir = x + "game\\0x52_URM.rpa"
+skip = 0
+modded = 0
+not_renpy = 0
 
-        # Check if the game is based on Ren'Py
-        if os.path.isdir(x + "renpy"):
-            pass
-        else:
-            not_renpy += 1
-            continue
+game_list = glob(game_dir + "\\*\\", recursive=True)
+for x in game_list:
+    game_name = x.split("\\")[-2]
+    dst_dir = x + "game\\0x52_URM.rpa"
 
-        # Check if the game folder structure is corrupted
-        if not os.path.isdir(x + "game"):
-            print("Error: Game folder corrupted for " + game_name)
-            skip += 1
-            continue
+    # Check if the game is based on Ren'Py
+    if os.path.isdir(x + "renpy"):
+        pass
+    else:
+        not_renpy += 1
+        continue
 
-        # Installing the mod if the game is not modded
-        if os.path.islink(dst_dir):
-            skip += 1
-        elif os.path.isfile(dst_dir):
-            os.remove(dst_dir)
-            print("Exact mod copy removed for " + game_name)
-            os.symlink(src_dir, dst_dir)
-            print("Symlink created for " + game_name)
-            modded += 1
-        else:
-            os.symlink(src_dir, dst_dir)
-            print("Symlink created for " + game_name)
-            modded += 1
-    print("Total games found: " + str(len(game_list)))
-    print("Total games modded: " + str(modded))
-    print("Total games skipped: " + str(skip))
-    print("Total non Ren'Py games: " + str(not_renpy))
+    # Check if the game folder structure is corrupted
+    if not os.path.isdir(x + "game"):
+        print("Error: Game folder corrupted for " + game_name)
+        skip += 1
+        continue
 
-
-if __name__ == "__main__":
-    main()
+    # Installing the mod if the game is not modded
+    if os.path.islink(dst_dir):
+        skip += 1
+    elif os.path.isfile(dst_dir):
+        os.remove(dst_dir)
+        print("Exact mod copy removed for " + game_name)
+        os.symlink(src_dir, dst_dir)
+        print("Symlink created for " + game_name)
+        modded += 1
+    else:
+        os.symlink(src_dir, dst_dir)
+        print("Symlink created for " + game_name)
+        modded += 1
+print("Total games found: " + str(len(game_list)))
+print("Total games modded: " + str(modded))
+print("Total games skipped: " + str(skip))
+print("Total non Ren'Py games: " + str(not_renpy))
